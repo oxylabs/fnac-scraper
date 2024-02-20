432a57e76523326f35e619e3ef350bc78ef93855
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:47:03 +0200

    Adding code examples/fnac_curl.sh

---
+++ code examples/fnac_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.fnacspectacles.com/?utm_campaign=onglet&utm_medium=text&utm_source=fnac.com&utm_term=billetterie"}'
